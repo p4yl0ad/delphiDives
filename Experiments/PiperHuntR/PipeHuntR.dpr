4d06728c412f78f8620b834b085630e7bf501168
p4yl0ad <p4yl0ad@protonmail.com>
2021-07-22 17:20:26 +0100

    PipeHuntR working 1st

--- Experiments/PiperHuntR/PipeHuntR.dpr
+++ Experiments/PiperHuntR/PipeHuntR.dpr
@@ -3,55 +3,31 @@ program PipeHuntR;
 {$APPTYPE CONSOLE}
 {$R *.res}
 
-// credits go to https://delphi.xcjc.net/viewthread.php?tid=49656&extra=page%3D2
-// and credits go https://gist.github.com/nstarke/02c57823f473702186e1db94dca8edae
-// Russell: pipes (use threads to read and write data via named pipes)
-// Solid code base to draw insp from
-
 uses
-  Windows, SysUtils, Classes, Messages;
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes, sysutils;
 
-
-
-// localized constant string declarations (Resource Strings)
-resourcestring
-  resPipeBaseName   =  '\\.\pipe\';
-  resPipeBaseFmtName=  '\\%s\pipe\';
-
-
+// START
+var
+  Info : TSearchRec;
+  ListDir : string;
 
 begin
-  try
-    //test comment here
-    //goal , enumerate local named pipes
-    WriteLn('PipeHuntR start');
-    //BAD http://www.delphibasics.co.uk/RTL.asp?Name=fileexists&ExpandCode1=Yes
-    //pipes as files
-
-
-
-    // START
-    var
-        Info : TSearchRec;
-
-    begin
-
-            if FindFirst(WorkingDir + '*.note', faAnyFile and faDirectory, Info)=0 then begin
-                    repeat
-                            DoSomething(WorkingDir, Info.Name);
-                    until FindNext(Info) <> 0;
-            end;
-            FindClose(Info);
-    end;
-    // END
-
-
-
-
-
-
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
+  ListDir:='\\.\pipe\';
+  If FindFirst (ListDir+'*',faAnyFile and faDirectory,Info)=0 then
+  //If FindFirst ('*',faAnyFile and faDirectory,Info)=0 then
+  begin
+    repeat
+      with Info do
+      begin
+        If (Attr and faDirectory) = faDirectory then
+          Write('Dir : ');
+        Writeln(ListDir+Name:40);
+      end;
+    until FindNext(info)<>0;
   end;
-end.
+  FindClose(Info);
+  Writeln ('Finished pipe hunt');
+  end.
