670d78308a4587121ae4dd462c8b8d3f921a65b9
p4yl0ad <p4yl0ad@protonmail.com>
2021-08-01 16:51:30 +0100

    added SeaShell_E

---
+++ SeaShell_E/SeaShell_E.dpr
@@ -0,0 +1,42 @@
+program SeaShell_E;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils,
+  IdComponent,
+  IdTCPConnection,
+  IdTCPClient;
+
+
+
+{
+https://github.com/vladon/IdTcpClientAndServerExample
+}
+
+
+
+
+
+begin
+var
+  IdTCPClient1: TIdTCPClient;
+
+  IdTCPClient1.Host := '127.0.0.1';
+  IdTCPClient1.Port := StrToInt('6666');
+  IdTCPClient1.Connect;
+
+
+  IdTCPClient1.IOHandler.WriteLn('[C1c] Hello Server');
+  IdTCPClient1.Disconnect;
+
+  try
+    WriteLn('kek');
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
